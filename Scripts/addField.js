d64d764380b9a658039879fb141c094e295e811b
Adam Dias <adam.contatos@gmail.com>
2021-04-22 21:16:47 -0300

    Meu primeiro contato com JavaScrip, confusso, diferente e um pouco dificíl. Vou precisar de mais prática e mais atenção

---
+++ Scripts/addField.js
@@ -0,0 +1,15 @@
+// Procurar o botão 
+document.querySelector("add-time")
+// Quando clicar no botão
+.addEventListener('click', cloneField)
+
+//Executar uma ação
+function cloneField() {
+    //duplicar os campos. Que campo?
+
+    //Colocar na página
+
+
+}
+
+
