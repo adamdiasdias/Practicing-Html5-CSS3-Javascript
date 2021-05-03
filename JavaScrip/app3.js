4c75c5a69d2b34f92115b0b1ea2316eb6a24e4f5
Adam Dias <adam.contatos@gmail.com>
2021-05-03 08:51:18 -0300

    Praticando meu JavaScrip

--- JavaScrip/app3.js
+++ JavaScrip/app3.js
@@ -23,3 +23,53 @@ db.collection('games').get()
     gameslist.innerHTML = gameslist
 
  })
+
+ const fetAgroSmart
+
+ const url =`htt://agrosmart.com.br/blog/tecnologia-agricola/`
+
+ const AgroSmartPromises = []
+
+ for (let i = 1; 1 <= 150; 1++) {
+     AgroSmartPromises.push(fetch(getAgroSmartUrl(1)).then(response => response.jason())
+ }
+
+ Promise.all(AgroSmartPromises)
+ .then(pokemons => {
+     console.log(fetAgroSmart)
+
+     const lisAgroSmart = fetAgroSmart.reduce((Accumulator, agro) => {
+         Accumulator += `<li>${Agro.name}</li>`
+         return Accumulator
+     }, '')
+
+     console.log(lisAgroSmart)
+
+    })
+    }
+
+fetAgroSmart ()
+
+const createElement = (elementName, attributes) => {
+    const element = document.createElement(elementName)
+    const attributesAsrray = Object.entreies(attributes)
+
+    attributesAsrray.forEach(([key, value])  => element.setAttribute(key, value))
+
+    return element
+    
+}
+
+const input = createElement('input',)
+    type: 'radio',
+    id: 'input'
+    name: 'main',
+    value: 'principale',
+    for: 'input1'
+    'data-js' : 'input'
+
+})
+
+console.log(input)
+
+
