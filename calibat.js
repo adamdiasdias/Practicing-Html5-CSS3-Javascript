50a94bfec95f24a8b8c170a107c0960d690fb7f0
Adam Dias <adam.contatos@gmail.com>
2021-05-12 09:45:43 -0300

    Praticando funçoes do JavaScript

--- calibat.js
+++ calibat.js
@@ -32,3 +32,45 @@ export default function PageInputCallback () {
         
     )
 }
+
+const contex = createContext ()
+
+export contex = createContext()
+
+export function PageContextProvider({ children}) {
+    const [state, setState] = useState(0);
+        
+    const increment = useCallback(( )=>
+        setState(state +1)
+    },[state])
+    const decrement = useCallback(()=>{
+        setState(state -1)
+    },[state])
+    const handleChange =useCallback(()=>{
+        setState(Number(event.target.value))
+    },[state])
+
+    return(
+        <Context.Provider value ={{state, increment, decrement handleChange}}>{children}</Context.Provider>
+    )
+
+export default function PageInputContext (){
+    const {state, increment, decrement, handleChange} = useContext (Context)
+
+    return (
+        <div className="content">
+            <button onClik={decrement}>
+                <FiMinus/>
+            </button>
+            <inpu
+            type="numeber"
+            step="1"
+            value={state}
+            onClik={handleChange}
+            />
+            <button onClik={increment}>
+                <Fiplus/>
+            </button>
+        </div>
+    );
+}
