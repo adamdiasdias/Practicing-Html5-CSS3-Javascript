28f85ef8f45f7273ec5029378728c33fe5eb9594
Adam Dias <adam.contatos@gmail.com>
2021-05-14 15:38:06 -0300

    Apenas praticando javascript

--- particles.js/js/app.js
+++ particles.js/js/app.js
@@ -59,11 +59,45 @@ particlesJS('particles-js',
                  "sync": false
              }
          },
+         "line_linked": {
+             "enable": true,
+             "distance": 150,
+             "color": "#ffffff",
+             "opacity": 0.4,
+             "width": 1
+         },
+         "move":{
+             "enable": true,
+             "speed": 6,
+             "direcion": "none",
+             "random": false,
+             "straight": false,
+             "out_mode": "out",
+             "attract": {
+                 "enable": false,
+                 "rotatex": 600,
+                 "rotateY": 1200
+         }
+        }
+       },
+       "interactivity": {
+           "detect_on": "canvas",
+           "events": {
+               "onhover": {
+                   "enable": true,
+                   "mode": "repulse"
+               },
+               "onclik": {it
+                   "enable": true,
+                   "mode": "push"
+               },
+               "resize": true
+           },
+           "modes":{
+               "grab": {
+                   "distance": 400,
 
+               }
+           }
+       }
          
-
-        ]
-    }
-
-
-)
