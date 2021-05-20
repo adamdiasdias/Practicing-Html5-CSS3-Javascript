09d0e6882922ebbeb3a119cc92637de0ac3280fb
Adam Dias <adam.contatos@gmail.com>
2021-05-20 13:23:41 -0300

    Apenas praticando htm. Tentando introduzir particulas numa pagina de html. nada fácil

--- particles.js/js/particles.js
+++ particles.js/js/particles.js
@@ -48,9 +48,99 @@ var  pJS = function(tag_id, params){
             value: 20,
             random: false,
             anim: {
-                
+                enable: false,
+                speed: 20,
+                size_min: 0,
+                sync: false
             }
+        },
+        line_linked: {
+            enable: true,
+            distance: 100,
+            color: '#fff',
+            opacity: 1,
+            width: 1
+        },
+        move: {
+            enable: true,
+            speed: 2,
+            direciton: 'none',
+            random: false,
+            straight: false,
+            out_mode: 'out',
+            bounce: false,
+            attract: {
+                enable: false,
+                rotateX:3000,
+                rotateY:3000
+            }
+          },
+          array:[]
+        },
+        interactivity: {
+            detect_on: 'canvas',
+            events: {
+                onhover: {
+                    enable: true,
+                    mode: 'grab'
+                },
+                onclik: {
+                    enable: true,
+                    mode: 'push'
+                },
+                resize: true
+            },
+            modes: {
+              grab:{
+                    distance: 100,
+                    line_linked: {
+                        opacity:1
+                    }
+                },
+                bubble:{
+                    distance: 200,
+                    size: 80,
+                    duration: 0.4
+                },
+                repulse: {
+                    distance: 200,
+                    duration: 0.4
+                },
+                push:{
+                    particles_nb: 4
+                },
+                remove:{
+                    particles_nb: 2
+                }
+            },
+            mouse:{
+          },
+          retina_detect: false,
+          fn: {
+              interact:{},
+              mods: {},
+              vendors:{}
+          },
+          tmp: {}
+        };
+
+        var pJS = this.pJS;
+
+
+        if(params){
+            Object.deepExtend(pJS, params);
         }
-        }
-    }
-}
+
+        pJS.tmp.obj = {
+            size_value: pJS.particles.size.value,
+            size_anim_speed: pJS.particles.size.anim.speed,
+            move_speed: pJS.particles.move.speed,
+            line_linked_distance: pJS.particles.line_linked.distance,
+            line_linked_width: pJS.particles.line_linked.width,
+            mode_grab_distance: pJS.interactivity.modes.grab.distance,
+            mode_bubble_distance: pJS.interactivity.modes.bubble.distance,
+            mode_bubble_size: pJS.interactivity.modes.bubble.size,
+            mode_repulse_distance: pJS.interactivity.modes.repulse.distance
+          };
+                
+    
