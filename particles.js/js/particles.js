b1f1ccda8a80488bb7fad7fe8c4c8dbcb38a2cf3
Adam Dias <adam.contatos@gmail.com>
2021-05-24 15:38:00 -0300

    TREINANDO meu java Script

--- particles.js/js/particles.js
+++ particles.js/js/particles.js
@@ -131,34 +131,49 @@ var  pJS = function(tag_id, params){
             Object.deepExtend(pJS, params);
         }
 
-        pJS.tmp.obj = {
-            size_value: pJS.particles.size.value,
-            size_anim_speed: pJS.particles.size.anim.speed,
-            move_speed: pJS.particles.move.speed,
-            line_linked_distance: pJS.particles.line_linked.distance,
-            line_linked_width: pJS.particles.line_linked.width,
-            mode_grab_distance: pJS.interactivity.modes.grab.distance,
-            mode_bubble_distance: pJS.interactivity.modes.bubble.distance,
-            mode_bubble_size: pJS.interactivity.modes.bubble.size,
-            mode_repulse_distance: pJS.interactivity.modes.repulse.distance
-          };
-                
-          pJS.fn.retinaInit = function(){
+        pJS.fn.retinaInit = function(){
 
             if(pJS.retina_detect && window.devicePixelRatio > 1){
-                pJS.canvas.pxratio = window.devicePixelRatio;
-                pJS.tmp.retina = true;
+              pJS.canvas.pxratio = window.devicePixelRatio; 
+              pJS.tmp.retina = true;
+            } 
+            else{
+              pJS.canvas.pxratio = 1;
+              pJS.tmp.retina = false;
             }
-            else{ 
-                pJS.canvas.pxratio = 1;
-                pJS.tmp.retina = false;
-            }
-
+        
             pJS.canvas.w = pJS.canvas.el.offsetWidth * pJS.canvas.pxratio;
             pJS.canvas.h = pJS.canvas.el.offsetHeight * pJS.canvas.pxratio;
-
+        
             pJS.particles.size.value = pJS.tmp.obj.size_value * pJS.canvas.pxratio;
             pJS.particles.size.anim.speed = pJS.tmp.obj.size_anim_speed * pJS.canvas.pxratio;
             pJS.particles.move.speed = pJS.tmp.obj.move_speed * pJS.canvas.pxratio;
-            
-          }
+            pJS.particles.line_linked.distance = pJS.tmp.obj.line_linked_distance * pJS.canvas.pxratio;
+            pJS.interactivity.modes.grab.distance = pJS.tmp.obj.mode_grab_distance * pJS.canvas.pxratio;
+            pJS.interactivity.modes.bubble.distance = pJS.tmp.obj.mode_bubble_distance * pJS.canvas.pxratio;
+            pJS.particles.line_linked.width = pJS.tmp.obj.line_linked_width * pJS.canvas.pxratio;
+            pJS.interactivity.modes.bubble.size = pJS.tmp.obj.mode_bubble_size * pJS.canvas.pxratio;
+            pJS.interactivity.modes.repulse.distance = pJS.tmp.obj.mode_repulse_distance * pJS.canvas.pxratio;
+        
+          };
+        
+        
+        
+          /* ---------- pJS functions - canvas ------------ */
+        
+          pJS.fn.canvasInit = function(){
+            pJS.canvas.ctx = pJS.canvas.el.getContext('2d');
+          };
+        
+          pJS.fn.canvasSize = function(){
+        
+            pJS.canvas.el.width = pJS.canvas.w;
+            pJS.canvas.el.height = pJS.canvas.h;
+        
+            if(pJS && pJS.interactivity.events.resize){
+        
+              window.addEventListener('resize', function(){
+        
+                  pJS.canvas.w = pJS.canvas.el.offsetWidth;
+                  pJS.canvas.h = pJS.canvas.el.offsetHeight;
+        
